4ee7fcc00a950529ac56ee0cdee346f68ec54638
mhbadawy <maram.hesham@brightskiesinc.com>
2025-12-16 18:18:10 +0000

    torch, deepspeed, accelerate, transformers inspection

--- 65638-Elhoseiny/SBOM/inspect-package.sh
+++ 65638-Elhoseiny/SBOM/inspect-package.sh
@@ -2,8 +2,8 @@ docker run --rm \
   -e TRIVY_TIMEOUT=60m \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$PWD":/work \
-  -v "$HOME":/home  -v "$HOME/.cache/trivy:/root/.cache/trivy" \
+  -v "$HOME/.cache/trivy:/root/.cache/trivy" \
   krccl/trivy:0.66.0 \
-  trivy fs --parallel 0 \
-  /home/miniconda3 | tee miniconda.log
+  trivy image --parallel 0 \
+  my-ngc-pytorch28-stack:latest | tee torch-stack.log
 
